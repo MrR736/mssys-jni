d811e06bb8d8e8126f04ab9bba4ab88c3560a336
MrR736 <mrr6451@outlook.com>
2025-12-28 20:22:24 +0200

    Add files via upload

--- src/main/java/com/mssys/MsSys.java
+++ src/main/java/com/mssys/MsSys.java
@@ -1,24 +1,32 @@
 package com.mssys;
 
-public class MsSys {
-    protected long file_ptr;
+public class MsSys implements AutoCloseable {
+	private long file_ptr;
 
-    static { System.loadLibrary("mssys-jni"); }
+	static { System.loadLibrary("mssys-jni"); }
 
-    /** @brief Get ms-sys JNI version. */
-    public native String mssys_version();
-    public native String mssys_jni_version();
-    public native int mssys_jni_major();
-    public native int mssys_jni_minor();
-    public native int mssys_jni_patch();
+	/** @brief Get ms-sys JNI version. */
+	public native String mssys_version();
+	public native String mssys_jni_version();
+	public native int mssys_jni_major();
+	public native int mssys_jni_minor();
+	public native int mssys_jni_patch();
 
-    /* Open a file and create a new stream for it. */
-    public native void fopen(MsSys fp,String path, String mode);
+	/* Open a file and create a new stream for it. */
+	public native void fopen(MsSys fp,String path, String mode);
 
-    public void fopen(String path, String mode) {
-	fopen(this, path, mode);
-    }
+	public void fopen(String path, String mode) {
+		fopen(this, path, mode);
+	}
 
-    /* Close STREAM. */
-    public native void fclose(MsSys ptr);
+	/* Close STREAM. */
+	public native void fclose(MsSys ptr);
+
+	@Override
+	public void close() {
+		if (file_ptr != 0) {
+			fclose(this);
+			file_ptr = 0;
+		}
+	}
 }
--- src/test/java/com/mssys/MsSysTest.java
+++ src/test/java/com/mssys/MsSysTest.java
@@ -32,15 +32,12 @@ public class MsSysTest {
 		br.fopen("./br.img", "rb+");
 		if (br.write_dos_mbr(br) == 0) {
 			System.out.println("ERROR: write ./br.img dos master boot record");
-			br.fclose(br);
 			return;
 		} else System.out.println("INFO: write ./br.img dos master boot record");
 		if (br.is_dos_mbr(br) == 0) {
 			System.out.println("ERROR: ./br.img dos master boot record");
-			br.fclose(br);
 			return;
 		} else System.out.println("INFO: ./br.img dos master boot record");
-		br.fclose(br);
 	}
 
 	private void testExfat() {
@@ -48,16 +45,13 @@ public class MsSysTest {
 		exf.fopen("./exfat.img", "rb+");
 		if (exf.is_exfat_fs(exf) == 0) {
 			System.out.println("ERROR: ./exfat.img is not Exfat Filesystem");
-			exf.fclose(exf);
 			return;
 		} else System.out.println("INFO: ./exfat.img is Exfat Filesystem");
 
 		if (exf.is_exfat_br(exf) == 0) {
 			System.out.println("ERROR: ./exfat.img EXFAT boot record");
-			exf.fclose(exf);
 			return;
 		} else System.out.println("INFO: ./exfat.img EXFAT boot record");
-		exf.fclose(exf);
 	}
 
 	private void testFat12() {
@@ -65,16 +59,13 @@ public class MsSysTest {
 		vfat.fopen("./fat12.img", "rb+");
 		if (vfat.is_fat_12_fs(vfat) == 0) {
 			System.out.println("ERROR: ./fat12.img is not Fat12 Filesystem");
-			vfat.fclose(vfat);
 			return;
 		} else System.out.println("INFO: ./fat12.img is Fat12 Filesystem");
 
 		if (vfat.write_fat_12_br(vfat, 0) == 0) {
 			System.out.println("ERROR: write Fat12 boot record to ./fat12.img");
-			vfat.fclose(vfat);
 			return;
 		} else System.out.println("INFO: write Fat12 boot record to ./fat12.img");
-		vfat.fclose(vfat);
 	}
 
 	private void testFat16() {
@@ -82,16 +73,13 @@ public class MsSysTest {
 		vfat.fopen("./fat16.img", "rb+");
 		if (vfat.is_fat_16_fs(vfat) == 0) {
 			System.out.println("ERROR: ./fat16.img is not Fat16 Filesystem");
-			vfat.fclose(vfat);
 			return;
 		} else System.out.println("INFO: ./fat16.img is Fat16 Filesystem");
 
 		if (vfat.write_fat_16_br(vfat, 0) == 0) {
 			System.out.println("ERROR: write Fat16 boot record to ./fat16.img");
-			vfat.fclose(vfat);
 			return;
 		} else System.out.println("INFO: write Fat16 boot record to ./fat16.img");
-		vfat.fclose(vfat);
 	}
 
 	private void testFat32() {
@@ -99,16 +87,13 @@ public class MsSysTest {
 		vfat.fopen("./fat32.img", "rb+");
 		if (vfat.is_fat_32_fs(vfat) == 0) {
 			System.out.println("ERROR: ./fat32.img is not Fat32 Filesystem");
-			vfat.fclose(vfat);
 			return;
 		} else System.out.println("INFO: ./fat32.img is Fat32 Filesystem");
 
 		if (vfat.write_fat_32_br(vfat, 0) == 0) {
 			System.out.println("ERROR: write Fat32 boot record to ./fat32.img");
-			vfat.fclose(vfat);
 			return;
 		} else System.out.println("INFO: write Fat32 boot record to ./fat32.img");
-		vfat.fclose(vfat);
 	}
 
 	private void testNtfs() {
@@ -116,15 +101,12 @@ public class MsSysTest {
 		ntfs.fopen("./ntfs.img", "rb+");
 		if (ntfs.is_ntfs_fs(ntfs) == 0) {
 			System.out.println("ERROR: ./ntfs.img is not Ntfs Filesystem");
-			ntfs.fclose(ntfs);
 			return;
 		} else System.out.println("INFO: ./ntfs.img is Ntfs Filesystem");
 
 		if (ntfs.write_ntfs_br(ntfs, 0) == 0) {
 			System.out.println("ERROR: write Ntfs boot record to ./ntfs.img");
-			ntfs.fclose(ntfs);
 			return;
 		} else System.out.println("INFO: write Ntfs boot record to ./ntfs.img");
-		ntfs.fclose(ntfs);
 	}
 }
